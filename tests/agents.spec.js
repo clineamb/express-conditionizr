7ee9356eb3aa67e841040e480901fb303f315c3f
Caroline Amaba <camaba@vaynermedia.com>
2015-02-26 14:12:22 -0500

    start adding tests

--- tests/agents.spec.js
+++ tests/agents.spec.js
@@ -1,1 +1,14 @@
 "use strict";
+
+var expect      = require('chai').expect
+,   detective   = require('../lib/detect_package.js')
+;
+
+//  ===== AGENT TESTS
+//  Make sure the User Agent string tests are actually working.
+
+describe("Agent Tests", function() {
+
+    describe
+
+});
